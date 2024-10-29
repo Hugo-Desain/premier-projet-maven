59969346ab2d361368a9ee8f30f7da3af106b086
hdesain-diginamic <hugodesain@gmail.com>
2024-10-29 15:46:29 +0100

    TP Maven 02 terminé

---
+++ src/main/java/dev/App.java
@@ -0,0 +1,9 @@
+package dev;
+
+import com.github.lalyos.jfiglet.FigletFont;
+
+public class App {
+    public static void main(String[] args) {
+        String asciiArt = FigletFont.convertOneLine("App");
+        System.out.println(asciiArt);    }
+}
